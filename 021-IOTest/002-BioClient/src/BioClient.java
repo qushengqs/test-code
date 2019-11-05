794f3579ba203e1cb96c0d1036a72e47086cf47d
senne <qu-sheng@shhe.com>
2019-11-05 10:52:03 +0800

    Bio 客户端代码

---
+++ 021-IOTest/002-BioClient/src/BioClient.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class BioClient {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket=new Socket("127.0.0.1",8080);
+
+            socket.getOutputStream().write("向服务器发送数据".getBytes());
+            socket.close();
+        }catch (IOException ex ){
+            ex.printStackTrace();
+        }
+
+    }
+}
