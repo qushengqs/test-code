5b9c086467b0512130f1f7af7a1bfe2766838a83
senne <qu-sheng@shhe.com>
2019-11-05 10:53:04 +0800

    格式化代码

--- 021-IOTest/002-BioClient/src/BioClient.java
+++ 021-IOTest/002-BioClient/src/BioClient.java
@@ -6,11 +6,11 @@ public class BioClient {
     public static void main(String[] args) {
 
         try {
-            Socket socket=new Socket("127.0.0.1",8080);
+            Socket socket = new Socket("127.0.0.1", 8080);
 
             socket.getOutputStream().write("向服务器发送数据".getBytes());
             socket.close();
-        }catch (IOException ex ){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
 
